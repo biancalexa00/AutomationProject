78e38b736ccb7a8da3e620cc7bfdaa6cdeda5cb6
biancalexa00 <biancalexa00@gmail.com>
2024-04-09 18:15:43 +0300

    am pus mai multe logs

--- src/test/java/Pages/FramePage.java
+++ src/test/java/Pages/FramePage.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,8 +17,13 @@ public class FramePage extends BasePage {
     public void dealWithIFrame() {
 
         frameMethods.switchSpecificIFrame("frame1");
+        LoggerUtility.infoTest("The user switch to a specific IFrame");
+
         frameMethods.switchParentFrame();
+        LoggerUtility.infoTest("The user switch to default IFrame");
+
         frameMethods.switchSpecificIFrame("frame2");
+        LoggerUtility.infoTest("The user switch to a specific IFrame");
 
     }
 
--- src/test/java/Tests/FrameTest.java
+++ src/test/java/Tests/FrameTest.java
@@ -1,26 +1,12 @@
 package Tests;
 
-import HelperMethod.ElementMethods;
-import HelperMethod.FrameMethods;
 import Pages.AlertsWindowFramePage;
 import Pages.FramePage;
 import Pages.HomePage;
-import SharedData.SharedData;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import SharedData.Hooks;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class FrameTest extends SharedData {
-
-//    public WebDriver webDriver;
+public class FrameTest extends Hooks {
 
     @Test
     public void frameMethod() {
